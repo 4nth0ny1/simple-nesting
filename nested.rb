1bcca9a05faa718d54a28da1a0c3ef12c4351eb0
4anth0ny1 <acatullo4@gmail.com>
2020-12-19 14:24:29 -0600

    commit tree 07dfdf5c852e0ea761d51be51993fc96731b18ca

--- nested.rb
+++ nested.rb
@@ -66,15 +66,9 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
-
 	programmer_hash = 
  		{
-
-        :yukihiro_matsumoto => {
-          :known_for => "Ruby",
-          :languages => ["LISP", "C"]
-        },
+        
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -88,9 +82,9 @@ def adding_matz
           :languages => ["C"]
         }
      }
-     
-
-end
+     programmer_hash[:yukihiro_matsumoto] ={}
+     programmer_hash
+    end
 
 
 def changing_alan
